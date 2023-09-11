0412847b2d1bfeda7524adca898c03e4b1d01fbe
Jaime-prog <a00571842@tec.mx>
2023-09-10 22:16:11 -0600

    agrega grafica del cambio del error cuadratico

--- Framework_LinearRegression/LinearRegression.py
+++ Framework_LinearRegression/LinearRegression.py
@@ -3,9 +3,6 @@ import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
 import seaborn as sns
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score, mean_squared_error
-
 
 # Importar el dataset
 dataset = pd.read_csv('Framework_LinearRegression/insurance.csv')
